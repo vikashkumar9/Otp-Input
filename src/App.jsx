4ac3ebd66aa89e3a6ae654cfd51d6d317c8d9e0a
Vikash <vikashparjapati59@gmail.com>
2024-03-05 16:51:05 +0530

    update otp

--- src/App.jsx
+++ src/App.jsx
@@ -4,32 +4,30 @@ import "./App.css";
 
 function App() {
   const [otp, setOtp] = useState(new Array(5).fill(""));
+  const [otplength, setOtplength] = useState(0);
 
   const clearOtp = () => {
     setOtp(new Array(5).fill(""));
   };
-
   const getOtp = () => {
     alert(otp.join(""));
   };
-
   const handleChange = (index, value) => {
     const newOtp = [...otp];
     newOtp[index] = value;
     setOtp(newOtp);
+    setOtplength(index + 1);
 
-    for (let i = index + 1; i < otp.length; i++) {
-      document.getElementById(`otpInput${i}`).disabled = true;
-    }
-    if (value) {
+    if (value && index < otp.length - 1) {
       document.getElementById(`otpInput${index + 1}`).disabled = false;
       document.getElementById(`otpInput${index + 1}`).focus();
     }
+
     if (value === "" && index > 0) {
       document.getElementById(`otpInput${index - 1}`).focus();
+      document.getElementById(`otpInput${index}`).disabled = true;
     }
   };
-
   return (
     <>
       <h2>Enter verification code</h2>
@@ -44,6 +42,7 @@ function App() {
                 onChange={(e) => handleChange(i, e.target.value)}
                 className="otp_inputs"
                 id={`otpInput${i}`}
+                disabled={otplength >= i ? false : true}
               />
             </div>
           );
